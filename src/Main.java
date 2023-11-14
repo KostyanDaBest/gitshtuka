c0ca8320fb11c96db0439cfc0f1eeb0e994018af
kosty <round.mc@yandex.by>
2023-11-13 19:05:04 -0500

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
             System.out.println( "change 1");
+            System.out.println( "change 2");
 
             System.out.println("i = " + i);
         }
